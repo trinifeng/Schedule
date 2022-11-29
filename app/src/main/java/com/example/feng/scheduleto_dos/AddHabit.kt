f7c8dc743dce93e16cd7dd7ef600d9cc20bec209
trini.feng23 <trini.feng23@d155.org>
2022-11-29 07:29:26 -0600

    Initial commit

---
+++ app/src/main/java/com/example/feng/scheduleto_dos/AddHabit.kt
@@ -0,0 +1,11 @@
+package com.example.feng.scheduleto_dos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddHabit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_habit)
+    }
+}
---
+++ app/src/main/java/com/example/feng/scheduleto_dos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.feng.scheduleto_dos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
