a6b79216179f9484c7a55f33536b3f78708c1488
trini.feng23 <trini.feng23@d155.org>
2022-11-29 08:09:14 -0600

    filled in AddHabit screen, created LoginScreen 11/29

--- app/src/main/java/com/example/feng/scheduleto_dos/AddHabit.kt
+++ app/src/main/java/com/example/feng/scheduleto_dos/AddHabit.kt
@@ -1,11 +1,24 @@
 package com.example.feng.scheduleto_dos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AddHabit : AppCompatActivity() {
+
+    lateinit var addHabitButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_habit)
+
+        addHabitButton = findViewById(R.id.add_habit_button)
+
+        addHabitButton.setOnClickListener {
+            Intent(this@AddHabit, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/feng/scheduleto_dos/MainActivity.kt
+++ app/src/main/java/com/example/feng/scheduleto_dos/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.feng.scheduleto_dos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var backButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        backButton = findViewById(R.id.back_button)
+
+        backButton.setOnClickListener {
+            Intent(this@MainActivity, AddHabit::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
