63ea75df24766e092e6be3e3da42bdf6d49df6ba
trini.feng23 <trini.feng23@d155.org>
2022-11-30 08:09:57 -0600

    data validation for LoginScreen

--- app/src/main/java/com/example/feng/scheduleto_dos/AddHabit.kt
+++ app/src/main/java/com/example/feng/scheduleto_dos/AddHabit.kt
@@ -4,16 +4,64 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ToggleButton
 
 class AddHabit : AppCompatActivity() {
 
     lateinit var addHabitButton: Button
+    lateinit var habitName: EditText
+    lateinit var habitTime: EditText
+    lateinit var timeButton: ToggleButton
+    lateinit var sessions: EditText
+    lateinit var mondayCheck: CheckBox
+    lateinit var tuesdayCheck: CheckBox
+    lateinit var wednesdayCheck: CheckBox
+    lateinit var thursdayCheck: CheckBox
+    lateinit var fridayCheck: CheckBox
+    lateinit var saturdayCheck: CheckBox
+    lateinit var sundayCheck: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_habit)
 
         addHabitButton = findViewById(R.id.add_habit_button)
+        habitName = findViewById(R.id.habit_name_text)
+        habitTime = findViewById(R.id.habit_time)
+        timeButton = findViewById(R.id.minutes_or_hours)
+        sessions = findViewById(R.id.num_sessions)
+        mondayCheck = findViewById(R.id.monday_check)
+        tuesdayCheck = findViewById(R.id.tuesday_check)
+        wednesdayCheck = findViewById(R.id.wednesday_check)
+        thursdayCheck = findViewById(R.id.thursday_check)
+        fridayCheck = findViewById(R.id.friday_check)
+        saturdayCheck = findViewById(R.id.saturday_check)
+        sundayCheck = findViewById(R.id.sunday_check)
+
+        var days = arrayListOf<Int>()
+        if(sundayCheck.isChecked) {
+            days.add(1)
+        }
+        if(mondayCheck.isChecked) {
+            days.add(2)
+        }
+        if(tuesdayCheck.isChecked) {
+            days.add(3)
+        }
+        if(wednesdayCheck.isChecked) {
+            days.add(4)
+        }
+        if(thursdayCheck.isChecked) {
+            days.add(5)
+        }
+        if(fridayCheck.isChecked) {
+            days.add(6)
+        }
+        if(saturdayCheck.isChecked) {
+            days.add(7)
+        }
 
         addHabitButton.setOnClickListener {
             Intent(this@AddHabit, MainActivity::class.java).also {
--- app/src/main/java/com/example/feng/scheduleto_dos/LoginScreen.kt
+++ app/src/main/java/com/example/feng/scheduleto_dos/LoginScreen.kt
@@ -3,22 +3,66 @@ package com.example.feng.scheduleto_dos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginScreen : AppCompatActivity() {
 
     lateinit var loginButton: Button
+    lateinit var loginText: EditText
+    lateinit var passwordText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
 
         loginButton = findViewById(R.id.login_button)
+        loginText = findViewById(R.id.username_edit_text)
+        passwordText = findViewById(R.id.password_edit_text)
 
         loginButton.setOnClickListener{
-            Intent(this@LoginScreen, AddHabit::class.java).also {
-                startActivity(it)
+            when(CheckLogin(loginText.text.toString(), passwordText.text.toString())) {
+
+                LoginSuccess.login -> {
+                    Toast.makeText(applicationContext, getString(R.string.error_login), Toast.LENGTH_SHORT).show()
+                    loginText.error = "Invalid Username"
+                    loginText.requestFocus()
+                    loginText.selectAll()
+                }
+                LoginSuccess.password -> {
+                    Toast.makeText(applicationContext, getString(R.string.error_password), Toast.LENGTH_SHORT).show()
+                    passwordText.error = "Invalid Password"
+                    passwordText.requestFocus()
+                    passwordText.selectAll()
+                }
+                LoginSuccess.success -> {
+                    Toast.makeText(applicationContext, getString(R.string.success), Toast.LENGTH_SHORT).show()
+                    Intent(this@LoginScreen, AddHabit::class.java).also {
+                        startActivity(it)
+                    }
+                }
             }
         }
+    }
+
+    fun CheckLogin(login: String, password: String): LoginSuccess {
+        val holdLogin = "Trini"
+        val holdPassword = "password"
+
+        if(holdLogin != login) {
+            return LoginSuccess.login
+        }
+        if(holdPassword != password) {
+            return LoginSuccess.password
+        }
+        return LoginSuccess.success
+    }
+
+    enum class LoginSuccess(val intValue: Int) {
+        login(1),
+        password(2),
+        success(0)
     }
 }
