00ed0a54ec33410d787cb79f73291aa9ec234734
trinif <trinilaurenf@gmail.com>
2022-11-29 20:50:17 -0600

    added login screen displays 11/29

--- app/src/main/java/com/example/feng/scheduleto_dos/LoginScreen.kt
+++ app/src/main/java/com/example/feng/scheduleto_dos/LoginScreen.kt
@@ -1,11 +1,24 @@
 package com.example.feng.scheduleto_dos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginScreen : AppCompatActivity() {
+
+    lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+
+        loginButton = findViewById(R.id.login_button)
+
+        loginButton.setOnClickListener{
+            Intent(this@LoginScreen, AddHabit::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
