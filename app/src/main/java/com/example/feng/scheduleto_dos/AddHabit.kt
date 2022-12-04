45de2280736cfc9113ff1836c1ef283f486fa968
trinif <trinilaurenf@gmail.com>
2022-12-03 21:33:17 -0600

    finished data validation 12/3

--- app/src/main/java/com/example/feng/scheduleto_dos/AddHabit.kt
+++ app/src/main/java/com/example/feng/scheduleto_dos/AddHabit.kt
@@ -3,10 +3,7 @@ package com.example.feng.scheduleto_dos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.ToggleButton
+import android.widget.*
 
 class AddHabit : AppCompatActivity() {
 
@@ -22,6 +19,11 @@ class AddHabit : AppCompatActivity() {
     lateinit var fridayCheck: CheckBox
     lateinit var saturdayCheck: CheckBox
     lateinit var sundayCheck: CheckBox
+
+    var habit = ""
+    var time = 0
+    var minutesOrHours = false
+    var numSessions = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,33 +42,99 @@ class AddHabit : AppCompatActivity() {
         saturdayCheck = findViewById(R.id.saturday_check)
         sundayCheck = findViewById(R.id.sunday_check)
 
-        var days = arrayListOf<Int>()
-        if(sundayCheck.isChecked) {
-            days.add(1)
-        }
-        if(mondayCheck.isChecked) {
-            days.add(2)
-        }
-        if(tuesdayCheck.isChecked) {
-            days.add(3)
-        }
-        if(wednesdayCheck.isChecked) {
-            days.add(4)
-        }
-        if(thursdayCheck.isChecked) {
-            days.add(5)
-        }
-        if(fridayCheck.isChecked) {
-            days.add(6)
-        }
-        if(saturdayCheck.isChecked) {
-            days.add(7)
-        }
-
         addHabitButton.setOnClickListener {
-            Intent(this@AddHabit, MainActivity::class.java).also {
-                startActivity(it)
+            var days = arrayListOf<Int>()
+            if(sundayCheck.isChecked) {
+                days.add(1)
+            }
+            if(mondayCheck.isChecked) {
+                days.add(2)
+            }
+            if(tuesdayCheck.isChecked) {
+                days.add(3)
+            }
+            if(wednesdayCheck.isChecked) {
+                days.add(4)
+            }
+            if(thursdayCheck.isChecked) {
+                days.add(5)
+            }
+            if(fridayCheck.isChecked) {
+                days.add(6)
+            }
+            if(saturdayCheck.isChecked) {
+                days.add(7)
+            }
+
+            days.sort()
+            minutesOrHours = timeButton.isChecked //hours is true, minutes is false
+            habit = habitName.text.toString()
+
+            when(CheckIfBlank(habit, habitTime.text.toString(), sessions.text.toString())) {
+
+                HabitSuccess.habit -> {
+                    habitName.error = getString(R.string.error_habit)
+                    habitName.requestFocus()
+                    habitName.selectAll()
+                }
+                HabitSuccess.time -> {
+                    habitTime.error = getString(R.string.error_time)
+                    habitTime.requestFocus()
+                    habitTime.selectAll()
+                }
+                HabitSuccess.sessions -> {
+                    sessions.error = getString(R.string.error_sessions)
+                    sessions.requestFocus()
+                    sessions.selectAll()
+                }
+                HabitSuccess.success -> {
+                    time = habitTime.text.toString().toInt()
+                    if((minutesOrHours && time >= 24) || (!minutesOrHours && time >= 1440)) {
+                        Toast.makeText(applicationContext, "Time cannot exceed or be equal to total time in a day", Toast.LENGTH_SHORT).show()
+                        habitTime.requestFocus()
+                        habitTime.selectAll()
+                    }
+                    else {
+                        numSessions = sessions.text.toString().toInt()
+
+                        Toast.makeText(applicationContext, getString(R.string.habit_success), Toast.LENGTH_SHORT).show()
+                        var result = "Your habit is: $habit for $time"
+                        result += if(minutesOrHours)
+                            " hours "
+                        else
+                            " minutes "
+                        result += "for $numSessions sessions per week"
+                        if(days.isNotEmpty())
+                            result += " preferably on days: $days"
+
+                        Intent(this@AddHabit, MainActivity::class.java).also {
+                            it.putExtra("EXTRA_RESULT", result)
+                            startActivity(it)
+                        }
+                    }
+                }
             }
         }
+    }
+
+    fun CheckIfBlank(habit: String, time: String, sessions: String): HabitSuccess {
+
+        if(habit.isNullOrBlank()) {
+            return HabitSuccess.habit
+        }
+        if(time.isNullOrBlank()) {
+            return HabitSuccess.time
+        }
+        if(sessions.isNullOrBlank()) {
+            return HabitSuccess.sessions
+        }
+        return HabitSuccess.success
+    }
+
+    enum class HabitSuccess(val intValue: Int) {
+        habit(1),
+        time(2),
+        sessions(3),
+        success(0)
     }
 }
--- app/src/main/java/com/example/feng/scheduleto_dos/MainActivity.kt
+++ app/src/main/java/com/example/feng/scheduleto_dos/MainActivity.kt
@@ -4,16 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var backButton: Button
+    lateinit var resultOutput: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         backButton = findViewById(R.id.back_button)
+        resultOutput = findViewById(R.id.result_output)
+
+        val result = intent.getStringExtra("EXTRA_RESULT")
+
+        resultOutput.text = result
 
         backButton.setOnClickListener {
             Intent(this@MainActivity, AddHabit::class.java).also {
